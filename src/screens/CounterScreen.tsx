9d3f678f7fd505b84771bb58f0759dddd0c4f75e
Andres2D <andres2d1997.1206@gmail.com>
2022-02-02 15:00:03 -0500

    creating Fab component to the buttons

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Fab } from '../components/Fab';
 
 export const CounterScreen = () => {
 
@@ -10,23 +11,26 @@ export const CounterScreen = () => {
           <Text style={ styles.title }>Counter: {counter}
           </Text>
 
-          <TouchableOpacity
-            style={ styles.fabLocationBR }
-            onPress={() => setCounter(counter + 1)}  
-          >
-            <View style={ styles.fab }>
-                <Text style={ styles.fabText }>+1</Text>
-            </View>
-          </TouchableOpacity>
+          <Fab 
+            title="+1"
+            onPress={() => setCounter(counter + 1)}
+            position='left'
+          />
 
-          <TouchableOpacity
+          <Fab 
+            title="-1"
+            onPress={() => setCounter(counter - 1)}
+            position='right'
+          />
+
+          {/* <TouchableOpacity
             style={ styles.fabLocationBL }
             onPress={() => setCounter(counter - 1)}  
           >
             <View style={ styles.fab }>
                 <Text style={ styles.fabText }>-1</Text>
             </View>
-          </TouchableOpacity>
+          </TouchableOpacity> */}
       </View>
   );
 };
@@ -34,34 +38,12 @@ export const CounterScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     title: {
         fontSize: 40,
         textAlign: 'center',
-        top: -15
-    },
-    fabLocationBR: {
-        position: 'absolute',
-        bottom: 25,
-        right: 25
-    },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
-        left: 25
-    },
-    fab: {
-        backgroundColor: '#5856D6',
-        width: 60,
-        height: 60,
-        borderRadius: 100,
-        justifyContent: 'center'
-    },
-    fabText: {
-        color: 'white',
-        fontSize: 25,
-        fontWeight: 'bold',
-        alignSelf: 'center'
+        top: -15,
+        color: 'black'
     }
 })
