c3ae1ab615c6134ce555c9a87b92f93f4800c007
Andres2D <andres2d1997.1206@gmail.com>
2022-02-02 13:52:57 -0500

    folder structure and new hello world component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
 
 export const App = () => {
+
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center'
-    }}>
-      <Text style={{
-        fontSize: 45,
-        textAlign: 'center'
-      }}>
-        Hello World
-        </Text>
-    </View>
+    <HelloWorldScreen />
   );
 };
-
-export default App;
