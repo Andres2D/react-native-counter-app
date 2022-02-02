30ef7526c1bcb5360b91d16307cd1229c8e13dda
Andres2D <andres2d1997.1206@gmail.com>
2022-02-02 14:20:26 -0500

    separate styles on StyleSheet object

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const CounterScreen = () => {
 
     const [counter, setCounter] = useState(10);
 
   return (
-      <View style={{
-          flex: 1,
-          justifyContent: 'center'
-      }}>
-          <Text style={{
-              textAlign: 'center',
-              fontSize: 40,
-              top: -15
-            }}>Counter: {counter}
+      <View style={ styles.container }>
+          <Text style={ styles.title }>Counter: {counter}
           </Text>
 
           <TouchableOpacity
             onPress={() => setCounter(counter + 1)}  
           >
-            <View style={{
-                backgroundColor: 'red',
-                borderRadius: 100
-            }}>
+            <View style={ styles.buttonIncrease }>
                 <Text>+1</Text>
             </View>
           </TouchableOpacity>
       </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15
+    },
+    buttonIncrease: {
+        backgroundColor: 'red',
+        borderRadius: 100
+    }
+})
