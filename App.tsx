dbb0be1246f96a2860435ae72885d5df7349ecc3
Andres2D <andres2d1997.1206@gmail.com>
2022-02-03 10:55:44 -0500

    add box properties

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
 import { CounterScreen } from './src/screens/CounterScreen';
 import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { SafeAreaView } from 'react-native';
 
 export const App = () => {
 
   return (
-    // <HelloWorldScreen />
-    // <CounterScreen />
-    <BoxObjectModelScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      {/* <HelloWorldScreen />
+      <CounterScreen /> */}
+      <BoxObjectModelScreen />
+
+    </SafeAreaView>
   );
 };
